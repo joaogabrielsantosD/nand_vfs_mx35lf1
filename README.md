16d86d417d7fe7e6dd2ce0d525c01b6777ecb3b2
joaogabrielsantosD <joao.donato@ufpe.br>
2025-10-22 14:22:28 -0300

    FEAT: GET, FET READ ID and RESET operation complete

--- README.md
+++ README.md
@@ -2,20 +2,63 @@
 
 ## WRITE Operations
 
+- [x] Write Enable
+- [x] Write Disable
+
 ## Feature Operations
+
+- [x] GET Feature
+- [x] SET Feature
+- [x] Wait operation Done
 
 ## READ Operations
 
+- [ ] PAGE READ
+- [ ] QE Bit
+- [ ] RANDOM DATA READ
+- [ ] READ FROM CACHE X2
+- [ ] READ FROM CACHE X4
+- [ ] Page Read Cache Sequential
+- [ ] Page Read Cache End
+- [ ] Page Read Cache flow
+- [x] Read ID
+
 ## Parameter Page
+
+- [ ] Paremeter page structure
 
 ## UniqueID Page
 
+- [ ] UniqueID Page
+
 ## Internal ECC Status Read
+
+- [ ] Internal ECC Status Read
 
 ## Program Operations
 
+- [ ] Program load
+- [ ] Program load random data
+- [ ] program load x4
+- [ ] QUAD IO program random input
+- [ ] program execute
+
 ## Block Operations
+
+- [ ] Block Erase
 
 ## Feature Register
 
+- [ ] Block Protection Feature
+- [ ] Secure OTP
+- [ ] Status Register
+
 ## Software Algorithm
+
+- [ ] Invalid blocks
+- [ ] Bad Block Test Flow
+- [ ] Failure Phenomena for Read/Program/Erase Operations
+
+## DEVICE POWER-UP
+
+- [x] RESET_ID
--- src/MX35LF1.c
+++ src/MX35LF1.c
@@ -1,11 +1,9 @@
 #include "MX35LF1.h"
 
+#include "esp_timer.h"
 #include <string.h>
 
-#define MX35_CLOCK_FREQUENCY (10 * 1000 * 1000)  // 10 MHz
-
-#define SELECT_WP(pin)   (gpio_set_level(pin, 0))
-#define UNSELECT_WP(pin) (gpio_set_level(pin, 1))
+#define MX35_CLOCK_FREQUENCY (50 * 1000 * 1000)  // 50 MHz
 
 #if CONFIG_NAND_MX35_SPI2_BUS
 #    define SPI_BUS_HOST SPI2_HOST
@@ -16,158 +14,168 @@
 static const char *TAG = "NAND MX35";
 static nand_mx35_handle_t mx35_ctx;
 
-static esp_err_t _spi_write(uint8_t td, const uint8_t *cmd, const uint8_t len)
+#define MX35_SELECT   (gpio_set_level(mx35_ctx->cfg.spi_pins.cs_io, 0))
+#define MX35_UNSELECT (gpio_set_level(mx35_ctx->cfg.spi_pins.cs_io, 1))
+
+#define ENABLE_WP  (gpio_set_level(mx35_ctx->cfg.spi_pins.wp_io, 0))
+#define DISABLE_WP (gpio_set_level(mx35_ctx->cfg.spi_pins.wp_io, 1))
+
+#define ENABLE_HOLD  (gpio_set_level(mx35_ctx->cfg.spi_pins.hd_io, 0))
+#define DISABLE_HOLD (gpio_set_level(mx35_ctx->cfg.spi_pins.hd_io, 1))
+
+static esp_err_t spi_write_read(const uint8_t *cmd, const uint8_t len, uint8_t *rx)
 {
-    esp_err_t ret;
     if (len == 0)
         return ESP_ERR_INVALID_ARG;  //no need to send anything
 
-    // spi_device_acquire_bus(mx35_dev, portMAX_DELAY);
-
-    uint8_t rx[4];
-
     spi_transaction_t t = {
-        // .cmd = *(cmd + 0),
         .length = len * 8,
         .tx_buffer = cmd,
-        .rx_buffer = rx,
+        .flags = SPI_TRANS_USE_RXDATA,
     };
 
-    ret = spi_device_polling_transmit(mx35_ctx->spi, &t);  // Transmit!
-    // ret = spi_device_transmit(mx35_dev, &t);
-    assert(ret == ESP_OK);
+    MX35_SELECT;
+    esp_err_t ret = spi_device_polling_transmit(mx35_ctx->spi, &t);  // Transmit!
+    MX35_UNSELECT;
 
-    // spi_device_release_bus(mx35_dev);
+    ESP_LOGD(TAG, "RECEIVED: [0x%02X, 0x%02X, 0x%02X, 0x%02X]", t.rx_data[0], t.rx_data[1], t.rx_data[2], t.rx_data[3]);
 
-    ESP_LOGD(TAG, "RECEIVED: [0x%02X, 0x%02X, 0x%02X, 0x%02X]", rx[0], rx[1], rx[2], rx[3]);
+    if (rx != NULL)
+        memcpy(rx, t.rx_data, 4);
 
     return ret;
 }
 
-// static esp_err_t spi_write(nand_mx35_handle_t ctx, const uint8_t *cmd, const uint8_t len)
-// {
-//     esp_err_t ret;
-//     if (len == 0)
-//         return ESP_ERR_INVALID_ARG;  //no need to send anything
-
-//     // spi_device_acquire_bus(mx35_dev, portMAX_DELAY);
-
-//     uint8_t rx[4];
-
-//     spi_transaction_t t = {
-//         // .cmd = *(cmd + 0),
-//         .length = len * 8,
-//         .tx_buffer = cmd,
-//         .rx_buffer = rx,
-//     };
-
-//     ret = spi_device_polling_transmit(mx35_dev, &t);  // Transmit!
-//     // ret = spi_device_transmit(mx35_dev, &t);
-//     assert(ret == ESP_OK);
-
-//     // spi_device_release_bus(mx35_dev);
-
-//     ESP_LOGD(TAG, "RECEIVED: [0x%02X, 0x%02X, 0x%02X, 0x%02X]", rx[0], rx[1], rx[2], rx[3]);
-
-//     return ret;
-// }
-
-// static uint8_t spi_read()
-// {
-//     spi_transaction_t t;
-//     memset(&t, 0, sizeof(t));
-//     t.length = 8;
-//     t.tx_buffer = (void *) 0;
-//     t.flags = SPI_TRANS_USE_RXDATA;
-
-//     esp_err_t ret = spi_device_polling_transmit(mx35_dev, &t);
-//     assert(ret == ESP_OK);
-
-//     ESP_LOGD(TAG, "RECEIVED: [0x%02X, 0x%02X, 0x%02X, 0x%02X]", t.rx_data[0], t.rx_data[1], t.rx_data[2], t.rx_data[3]);
-
-//     return *(uint8_t *) t.rx_data;
-// }
-
-// static uint8_t nand_mx35lf_GET_Features(uint8_t address)
-// {
-//     ESP_LOGV(TAG, "uint8_t nand_mx35lf_GET_Features(...)");
-
-//     // uint8_t cmd[] = {CMD_GET_FEATURES, address};
-//     // spi_write(cmd, sizeof(cmd));
-//     // uint8_t ret = spi_read();
-
-//     return 1;
-// }
-
-// static void Test_GET_Registers_BlockProtection()
-// {
-//     ESP_LOGV(TAG, "void Test_GET_Registers_BlockProtection()");
-
-//     uint8_t reg = nand_mx35lf_GET_Features(REG_BLOCK_PROTECTION);
-//     printf("RESULT: 0x%02X\r\n", reg);
-
-//     printf("bit[0] - [SP]            - %d\r\n", (reg >> 0) & 0x01);
-//     printf("bit[1] - [Complementary] - %d\r\n", (reg >> 1) & 0x01);
-//     printf("bit[2] - [Invert]        - %d\r\n", (reg >> 2) & 0x01);
-//     printf("bit[3] - [BP0]           - %d\r\n", (reg >> 3) & 0x01);
-//     printf("bit[4] - [BP1]           - %d\r\n", (reg >> 4) & 0x01);
-//     printf("bit[5] - [BP2]           - %d\r\n", (reg >> 5) & 0x01);
-//     printf("bit[6] - [Reserved]      - %d\r\n", (reg >> 6) & 0x01);
-//     printf("bit[7] - [BPRWD]         - %d\r\n", (reg >> 7) & 0x01);
-//     printf("\r\n");
-// }
-
-// static void Test_GET_Registers_SecureOTP()
-// {
-//     ESP_LOGV(TAG, "void Test_GET_Registers_SecureOTP()");
-
-//     uint8_t reg = nand_mx35lf_GET_Features(REG_SECURE_OTP);
-//     printf("RESULT: 0x%02X\r\n", reg);
-
-//     printf("bit[0] - [QE]                 - %d\r\n", (reg >> 0) & 0x01);
-//     printf("bit[1] - [Reserved]           - %d\r\n", (reg >> 1) & 0x01);
-//     printf("bit[2] - [Reserved]           - %d\r\n", (reg >> 2) & 0x01);
-//     printf("bit[3] - [Reserved]           - %d\r\n", (reg >> 3) & 0x01);
-//     printf("bit[4] - [ECC enabled]        - %d\r\n", (reg >> 4) & 0x01);
-//     printf("bit[5] - [Reserved]           - %d\r\n", (reg >> 5) & 0x01);
-//     printf("bit[6] - [Secure OTP Enable]  - %d\r\n", (reg >> 6) & 0x01);
-//     printf("bit[7] - [Secure OTP Protect] - %d\r\n", (reg >> 7) & 0x01);
-//     printf("\r\n");
-// }
-
-// static void Test_GET_Registers_Status()
-// {
-//     ESP_LOGV(TAG, "void Test_GET_Registers_Status()");
-
-//     uint8_t reg = nand_mx35lf_GET_Features(REG_STATUS);
-//     printf("RESULT: 0x%02X\r\n", reg);
-
-//     printf("bit[0] - [OIP]      - %d\r\n", (reg >> 0) & 0x01);
-//     printf("bit[1] - [WEL]      - %d\r\n", (reg >> 1) & 0x01);
-//     printf("bit[2] - [E_Fail]   - %d\r\n", (reg >> 2) & 0x01);
-//     printf("bit[3] - [P-Fail]   - %d\r\n", (reg >> 3) & 0x01);
-//     printf("bit[4] - [ECC_S0]   - %d\r\n", (reg >> 4) & 0x01);
-//     printf("bit[5] - [ECC_S1]   - %d\r\n", (reg >> 5) & 0x01);
-//     printf("bit[6] - [CRBSY]    - %d\r\n", (reg >> 6) & 0x01);
-//     printf("bit[7] - [Reserved] - %d\r\n", (reg >> 7) & 0x01);
-//     printf("\r\n");
-// }
-
-static void mx35_reset(nand_mx35_handle_t ctx)
+static void nand_mx35lf_SET_Features(uint8_t address, uint8_t value)
 {
-    // Internal ECC status going to zero
-    SELECT_WP(ctx->cfg.spi_pins.wp_io);
+    ESP_LOGV(TAG, "void mx35lf_SET_features()");
 
-    uint8_t cmd[] = {CMD_RESET, 0xFF, 0xFF, 0xFF, 0xFF};
-    spi_transaction_t t = {.length = 8, .tx_buffer = cmd};
-
-    spi_device_polling_transmit(ctx->spi, &t);  // Transmit!
-
-    vTaskDelay(pdMS_TO_TICKS(2));
-    UNSELECT_WP(ctx->cfg.spi_pins.wp_io);
-
-    vTaskDelay(pdMS_TO_TICKS(250));
+    uint8_t cmd[] = {CMD_SET_FEATURES, address, value};
+    spi_write_read(cmd, sizeof(cmd), NULL);
+    return;
 }
+
+static uint8_t nand_mx35lf_GET_Features(uint8_t address)
+{
+    ESP_LOGV(TAG, "uint8_t nand_mx35lf_GET_Features(...)");
+
+    uint8_t cmd[] = {CMD_GET_FEATURES, address, 0x00};
+    uint8_t _rx[4];
+    spi_write_read(cmd, sizeof(cmd), _rx);
+    return _rx[2];
+}
+
+static mx35_err_t WaitOperationDone()
+{
+    uint32_t time_reference = esp_timer_get_time() / 1000;
+    const unsigned long timeout = 500;
+    while (((uint32_t) (esp_timer_get_time() / 1000) - time_reference) < timeout)
+        if ((nand_mx35lf_GET_Features(REG_STATUS) & 0x01) == STATUS_READY)
+            return MX35_OK;
+    return MX35_FAIL;
+}
+
+static bool nand_mx35_write_enable()
+{
+    uint8_t cmd[] = {CMD_WRITE_ENABLE};
+    uint8_t _rx[4];
+    spi_write_read(cmd, sizeof(cmd), _rx);
+
+    return _rx[2] & WEL_BIT;
+}
+
+static bool nand_mx35_write_disable()
+{
+    uint8_t cmd[] = {CMD_WRITE_DISABLE};
+    uint8_t _rx[4];
+    spi_write_read(cmd, sizeof(cmd), _rx);
+
+    return (_rx[2] & WEL_BIT) == 0x00;
+}
+
+static void nand_mx35_reset()
+{
+    /* reset the module */
+    spi_transaction_t trans_desc = {
+        .flags = SPI_TRANS_USE_TXDATA,
+        .tx_data = {CMD_RESET},
+        .length = 8,
+    };
+
+    MX35_SELECT;
+    spi_device_polling_transmit(mx35_ctx->spi, &trans_desc);
+    MX35_UNSELECT;
+
+    ENABLE_WP;
+    vTaskDelay(pdMS_TO_TICKS(2));
+    DISABLE_WP;
+}
+
+static bool nand_mx35_get_id()
+{
+    uint8_t cmd[] = {CMD_READ_MANUFACTURER_ID, 0xFF, 0xFF, 0xFF};
+    uint8_t _rx[4];
+    spi_write_read(cmd, sizeof(cmd), _rx);
+    return (_rx[2] == MANUFACTURER_ID && _rx[3] == DEVICE_ID);
+}
+
+#if CONFIG_NAND_MX35_DEBUG_GET_REG
+
+static void Test_GET_Registers_BlockProtection()
+{
+    ESP_LOGV(TAG, "void Test_GET_Registers_BlockProtection()");
+
+    uint8_t reg = nand_mx35lf_GET_Features(REG_BLOCK_PROTECTION);
+    printf("RESULT: 0x%02X\r\n", reg);
+
+    printf("bit[0] - [SP]            - %d\r\n", (reg >> 0) & 0x01);
+    printf("bit[1] - [Complementary] - %d\r\n", (reg >> 1) & 0x01);
+    printf("bit[2] - [Invert]        - %d\r\n", (reg >> 2) & 0x01);
+    printf("bit[3] - [BP0]           - %d\r\n", (reg >> 3) & 0x01);
+    printf("bit[4] - [BP1]           - %d\r\n", (reg >> 4) & 0x01);
+    printf("bit[5] - [BP2]           - %d\r\n", (reg >> 5) & 0x01);
+    printf("bit[6] - [Reserved]      - %d\r\n", (reg >> 6) & 0x01);
+    printf("bit[7] - [BPRWD]         - %d\r\n", (reg >> 7) & 0x01);
+    printf("\r\n");
+}
+
+static void Test_GET_Registers_SecureOTP()
+{
+    ESP_LOGV(TAG, "void Test_GET_Registers_SecureOTP()");
+
+    uint8_t reg = nand_mx35lf_GET_Features(REG_SECURE_OTP);
+    printf("RESULT: 0x%02X\r\n", reg);
+
+    printf("bit[0] - [QE]                 - %d\r\n", (reg >> 0) & 0x01);
+    printf("bit[1] - [Reserved]           - %d\r\n", (reg >> 1) & 0x01);
+    printf("bit[2] - [Reserved]           - %d\r\n", (reg >> 2) & 0x01);
+    printf("bit[3] - [Reserved]           - %d\r\n", (reg >> 3) & 0x01);
+    printf("bit[4] - [ECC enabled]        - %d\r\n", (reg >> 4) & 0x01);
+    printf("bit[5] - [Reserved]           - %d\r\n", (reg >> 5) & 0x01);
+    printf("bit[6] - [Secure OTP Enable]  - %d\r\n", (reg >> 6) & 0x01);
+    printf("bit[7] - [Secure OTP Protect] - %d\r\n", (reg >> 7) & 0x01);
+    printf("\r\n");
+}
+
+static void Test_GET_Registers_Status()
+{
+    ESP_LOGV(TAG, "void Test_GET_Registers_Status()");
+
+    uint8_t reg = nand_mx35lf_GET_Features(REG_STATUS);
+    printf("RESULT: 0x%02X\r\n", reg);
+
+    printf("bit[0] - [OIP]      - %d\r\n", (reg >> 0) & 0x01);
+    printf("bit[1] - [WEL]      - %d\r\n", (reg >> 1) & 0x01);
+    printf("bit[2] - [E_Fail]   - %d\r\n", (reg >> 2) & 0x01);
+    printf("bit[3] - [P-Fail]   - %d\r\n", (reg >> 3) & 0x01);
+    printf("bit[4] - [ECC_S0]   - %d\r\n", (reg >> 4) & 0x01);
+    printf("bit[5] - [ECC_S1]   - %d\r\n", (reg >> 5) & 0x01);
+    printf("bit[6] - [CRBSY]    - %d\r\n", (reg >> 6) & 0x01);
+    printf("bit[7] - [Reserved] - %d\r\n", (reg >> 7) & 0x01);
+    printf("\r\n");
+}
+
+#endif
 
 mx35_err_t nand_mx35_init(const nand_mx35_config_t *cfg)
 {
@@ -192,29 +200,26 @@ mx35_err_t nand_mx35_init(const nand_mx35_config_t *cfg)
         .sclk_io_num = (int) cfg->spi_pins.sclk_io,
         .quadhd_io_num = (int) cfg->spi_pins.hd_io,
         .quadwp_io_num = (int) cfg->spi_pins.wp_io,
-        .max_transfer_sz = 0,  // driver decides
+        .max_transfer_sz = 4095,
     };
 
     spi_device_interface_config_t devcfg = {
         .clock_speed_hz = MX35_CLOCK_FREQUENCY,
         .mode = 0,
         .spics_io_num = cfg->spi_pins.cs_io,
-        .queue_size = 128,
+        .queue_size = 10,
     };
 
     esp_err_t ret = spi_bus_initialize(SPI_BUS_HOST, &buscfg, SPI_DMA_CH_AUTO);
-
-    if (ret == ESP_OK || ret == ESP_ERR_INVALID_STATE)
-    {
-        ESP_LOGI(TAG, "SPI Initialize");
-        ret = spi_bus_add_device(SPI_BUS_HOST, &devcfg, &ctx->spi);
-    }
 
     if (ret)
     {
         ESP_LOGE(TAG, "Error to initialize the SPI bus");
         goto cleanup;
     }
+
+    ESP_LOGI(TAG, "SPI Initialize");
+    ret = spi_bus_add_device(SPI_BUS_HOST, &devcfg, &ctx->spi);
 
     mx35_ctx = ctx;
 
@@ -224,22 +229,28 @@ mx35_err_t nand_mx35_init(const nand_mx35_config_t *cfg)
     };
     gpio_config(&out_cfg);
 
-    gpio_set_level(cfg->spi_pins.cs_io, 0);
-    gpio_set_level(cfg->spi_pins.hd_io, 1);
-    UNSELECT_WP(cfg->spi_pins.wp_io);
+    MX35_UNSELECT;
+    DISABLE_WP;
+    DISABLE_HOLD;
 
-    // mx35_reset(ctx);
+    nand_mx35_reset();
+    if (!nand_mx35_get_id())
+    {
+        ESP_LOGE(TAG, "Dont communicate with the MX35 module");
+        goto cleanup;
+    }
 
-    // função de get_id
-    UNSELECT_WP(ctx->cfg.spi_pins.wp_io);
-    uint8_t cmd[] = {CMD_READ_MANUFACTURER_ID, 0xFF, 0xFF, 0xFF};
-    _spi_write(1, cmd, sizeof(cmd));
+    nand_mx35_write_disable();
 
-    // Test_GET_Registers_BlockProtection();
-    // Test_GET_Registers_SecureOTP();
-    // Test_GET_Registers_Status();
+#if CONFIG_NAND_MX35_DEBUG_GET_REG
+    Test_GET_Registers_BlockProtection();
+    Test_GET_Registers_SecureOTP();
+    Test_GET_Registers_Status();
+#endif
 
-    SELECT_WP(ctx->cfg.spi_pins.wp_io);
+    MX35_UNSELECT;
+    DISABLE_HOLD;
+    ENABLE_WP;
     return MX35_OK;
 
 cleanup:
@@ -255,6 +266,8 @@ cleanup:
 
 mx35_err_t nand_mx35_deinit()
 {
+    ESP_LOGV(TAG, "mx35_err_t nand_mx35_deinit()");
+
     if (mx35_ctx->spi)
         spi_bus_remove_device(mx35_ctx->spi);
 
